0f39eb16bec9283a748542161dce5f34c70a2e4e
becca <sunbecca@gmail.com>
2020-05-27 12:49:14 -0500

    add a picture of the earth

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I am left handed', 'My favorite food is raspberries!', 'I speak Chinese and am learning Spanish', 'I can solve a Rubik\'s Cube'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
