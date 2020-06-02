91b44820b1a309bb9cc9dc093306e3212849c9c9
becca <sunbecca@gmail.com>
2020-06-02 18:08:17 +0000

    used fetch to make get message button

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -2,3 +2,9 @@
 function scrollUp() {
     window.scrollTo({top:0, behavior: 'smooth'});
 }
+
+async function getGreeting() {
+  const response = await fetch('/data');
+  const greeting = await response.text();
+  document.getElementById('message-container').innerText = greeting;
+}
